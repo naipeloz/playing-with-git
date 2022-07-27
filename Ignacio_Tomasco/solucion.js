d55c1220b87cd46a397dec5d523e7a20c05c51b1
Ignacio Tomasco <102391202+Raggi37@users.noreply.github.com>
2022-07-27 19:53:41 -0300

    solucion.js

---
+++ Ignacio_Tomasco/solucion.js
@@ -0,0 +1,20 @@
+const dayQty = (month) => {
+    const thirtyDayMonth = ["april", "june", "september", "november"];
+    const thirtyOneDayMonth = [
+      "january",
+      "march",
+      "may",
+      "july",
+      "august",
+      "october",
+      "december",
+    ];
+    if (thirtyDayMonth.includes(month)) {
+      return 30;
+    }
+    if (thirtyOneDayMonth.includes(month)) {
+      return 31;
+    } else {
+      return 28;
+    }
+  };
