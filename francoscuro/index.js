af9e099575c1d970e47e578eadb75e66a0eaa05a
Franco Scuro <franquitoscro@gmail.com>
2022-07-27 19:54:24 -0300

    resolucion del ejercicio

---
+++ francoscuro/index.js
@@ -0,0 +1,34 @@
+const meses = [
+    {nombre: 'Enero', dias: 31},
+    {nombre: 'Febrero', dias: 28},
+    {nombre: 'Marzo', dias: 31},
+    {nombre: 'Abril', dias: 30},
+    {nombre: 'Mayo', dias: 31},
+    {nombre: 'Junio', dias: 31},
+    {nombre: 'Julio', dias: 30},
+    {nombre: 'Agosto', dias: 31},
+    {nombre: 'Setiembre', dias: 30},
+    {nombre: 'Octubre', dias: 31},
+    {nombre: 'Noviembre', dias: 30},
+    {nombre: 'Diciembre', dias: 31}
+]
+
+
+// const meses = {
+//     Enero: 31,
+//     Febrero: 28,
+//     Marzo: 31,
+//     Abril: 30,
+//     Mayo: 31,
+//     Junio: 31,
+//     Julio: 30,
+//     Agosto: 31,
+//     Setiembre: 30,
+//     Octubre: 31,
+//     Noviembre: 30,
+//     Diciembre: 31
+// }
+
+const diasMes = () => {
+    let nombreMes
+}
