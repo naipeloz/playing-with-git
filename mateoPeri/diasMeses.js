70682b9683bf23031a35f4c4c39aff4b4b56d7ae
MateoPeri <mateoperirufo@gmail.com>
2022-07-27 19:40:06 -0300

    mateoPeri

---
+++ mateoPeri/diasMeses.js
@@ -0,0 +1,14 @@
+ function diasMeses(n) {
+    if(n ==1 || n == 3 || n==5 || n == 7 || n==8 || n== 10 || n==12){
+      return 31;
+ } 
+ else if(n==4 || n==6 || n==9 || n==11){
+      return 30;
+ }
+ else if(n==2){
+    return 28;
+ }
+ else{
+    return "Error"
+ }
+ }
