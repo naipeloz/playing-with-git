9b4aa99d6f5687e5166a7fdff4f01e4a56f4bbd2
Facundo Martirena <facundo.martirena@estudiantes.utec.edu.uy>
2022-07-28 19:54:24 -0300

    v2

--- solucion/index.js
+++
@@ -1,9 +0,0 @@
-
-
-function piedraPapelTijera (algoritmo, jugador) {
-    const numeroAleatorio = (min,max) => {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-    console.log("valor: ", numeroAleatorio(1,4));
-    
-};
