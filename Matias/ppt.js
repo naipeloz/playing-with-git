753bc48de128a6d0e8376288cd9f72a6ed093ec5
Matias Garbi <matiasgarbi@gmail.com>
2022-07-27 21:49:00 -0300

    test arreglado

--- Matias/ppt.js
+++ Matias/ppt.js
@@ -1,26 +1,33 @@
-let pregunta = prompt("Ingresa tu opción: 1, 2 o 3 "); 
-let user = pregunta.toLowerCase();
-let options = ["1", "2", "3"];
-let machine = options[Math.floor(Math.random() * 3)];
+function ppt(){
 
-switch (true) {
-    case (machine === user):
-        console.log("Empataron");
+    let pregunta = prompt("Ingresa tu opción: 1, 2 o 3 "); 
+    let user = pregunta.toLowerCase();
+    let options = ["1", "2", "3"];
+    let machine = options[Math.floor(Math.random() * 3)];
+    
+    switch (true) {
+        case (machine === user):
+            console.log("Empataron");
+            break;
+            
+            case (machine === "1" && user ==="2"):
+                console.log("Ganaste!");
         break;
+        
+        case (machine === "2" && user === "3"):
+            console.log("Ganaste!");
+            break;
+            
+        case (machine === "3" && user === "1"):
+            console.log("Ganaste!");
+            break;
+                
+        default:
+            console.log("Perdiste!");
+            break;
+    }
 
-    case (machine === "1" && user ==="2"):
-        console.log("Ganaste!");
-        break;
-
-    case (machine === "2" && user === "3"):
-        console.log("Ganaste!");
-        break;
-
-    case (machine === "3" && user === "1"):
-        console.log("Ganaste!");
-        break;
-
-    default:
-        console.log("Perdiste!");
-        break;
+    if(options <1 || options>3){
+        return console.log("opcion incorrecta")
+    }
 }
--- Matias/ppt.test.js
+++ Matias/ppt.test.js
@@ -3,3 +3,7 @@ const ppt = require('./ppt');
 test('adds 1',() => {
     expect(ppt(1)).toBe("Ganaste!")
 });
+
+test('adds 6',() => {
+    expect(ppt(6)).toBe("opcion incorrecta")
+});
