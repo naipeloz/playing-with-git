323d55dd01564185e9e3d4abd36a384b691179ce
dariorod <dario2303@gmail.com>
2022-07-28 20:58:35 -0300

    PiedraPapelTijera

--- Juego.js
+++ Juego.js
@@ -1,36 +1,48 @@
-const Juego = (humano) => {
-    let maquina = Math.floor(Math.random() * 3);
-    console.log(maquina);
+const Juego = () => {
+
+  let humano = prompt('seleccione una de las siguientes opciones: 1=piedra, 2=papel y 3=tijera');
+  humano = parseInt(humano)
+  let maquina = Math.floor(Math.random() * (4 - 1) + 1);
   
-    if (maquina == 0) {
+    if (maquina === 1) {
       maquina = "piedra";
     }
-    if (maquina == 1) {
+    if (maquina === 2) {
       maquina = "papel";
-    } else {
+    }
+    if (maquina === 3) {
       maquina = "tijera";
     }
-  
-    if (
-      (humano === "tijera" && maquina === "piedra") ||
-      (humano === "papel" && maquina === "tijera") ||
-      (humano === "piedra" && maquina === "papel") 
-    ) {
-      return "pc wins!";
+
+    if (humano === 1) {
+      humano = "piedra";
     }
-    if (
-      (humano === "piedra" && maquina === "tijera") ||
-      (humano === "papel" && maquina === "piedra") ||
-      (humano === "tijera" && maquina === "papel")
-    ) {
-      return "human wins!";
+    if (humano === 2) {
+      humano = "papel";
+    }
+    if (humano === 3) {
+      humano = "tijera";
     }
 
-    if (humano === maquina) {
-        return "empate";
+    alert ('la maquina ha seleccionado:' + maquina)
+    alert ('tu has elegido:' + humano)
+
+    if (
+      ( humano == "tijera" && maquina == "piedra") ||
+      ( humano == "papel" && maquina == "tijera") ||
+      ( humano == "piedra" && maquina == "papel") 
+    ) {
+      return console.log("la maquina ha ganado");
+    }
+    if (
+      ( humano == "piedra" && maquina == "tijera") ||
+      ( humano == "papel" && maquina == "piedra") ||
+      ( humano == "tijera" && maquina == "papel")
+    ) {
+      return console.log("Has ganado! felicitaciones");
+    }
+    if (humano == maquina) {
+        return console.log("es un empate");
       }
-
-
   };
-  
-  Juego(2);
+  Juego();
