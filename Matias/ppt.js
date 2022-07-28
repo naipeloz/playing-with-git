56431f3d402635f87e5f76b9c4efc7b60454ff62
Matias Garbi <matiasgarbi@gmail.com>
2022-07-27 21:16:15 -0300

    se arreglan errores

--- Matias/ppt.js
+++ Matias/ppt.js
@@ -1,26 +1,26 @@
-let pregunta = prompt("Ingresa tu opción: piedra, papel o tijera "); 
+let pregunta = prompt("Ingresa tu opción: 1, 2 o 3 "); 
 let user = pregunta.toLowerCase();
-let options = ["piedra", "papel", "tijera"];
+let options = ["1", "2", "3"];
 let machine = options[Math.floor(Math.random() * 3)];
 
 switch (true) {
     case (machine === user):
-        alert("Empataron");
+        console.log("Empataron");
         break;
 
-    case (machine === "piedra" && user ==="papel"):
-        alert("Ganaste!");
+    case (machine === "1" && user ==="2"):
+        console.log("Ganaste!");
         break;
 
-    case (machine === "papel" && user === "tijera"):
-        alert("Ganaste!");
+    case (machine === "2" && user === "3"):
+        console.log("Ganaste!");
         break;
 
-    case (machine === "tijera" && user === "piedra"):
-        alert("Ganaste!");
+    case (machine === "3" && user === "1"):
+        console.log("Ganaste!");
         break;
 
     default:
-        alert("Perdiste!");
+        console.log("Perdiste!");
         break;
 }
