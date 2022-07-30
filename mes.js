b3dfb3856dae098cff225c285fe6d56aefc0adb6
dariorod <dario2303@gmail.com>
2022-07-29 21:42:57 -0300

    meses

---
+++ mes.js
@@ -0,0 +1,27 @@
+let monthSelect = 9;
+
+const month28 = 2;
+const month30 = [4,6,9,11];
+const month31 = [1,3,5,7,8,10,12];
+
+
+function month (select) {
+    if (select === month28){
+        console.log('el mes seleccionado tiene 28 días');
+    }
+    
+    for(i = 0; i <= monthSelect; i++){
+        if (select === month30[i]){
+            console.log('el mes seleccionado tiene 30 días');
+            }
+    }
+    
+    for(i = 0; i <= monthSelect; i++){
+        if (select === month31[i]){
+            console.log('el mes seleccionado tiene 31 días');
+            }
+        }
+
+}
+
+month(monthSelect)
